aeaaf72a2da7b0439042857bed48168ca38a6d2c
Aayu5h <aayushmagrawal@gmail.com>
2023-01-25 20:37:35 +0530

    Add files via upload

---
+++ _nuxt/Tag.vue_vue_type_script_setup_true_lang.b0a586cb.js
@@ -0,0 +1,10 @@
+import{m as s,a as t,c as n,q as a,e as o}from"./entry.2dc74bde.js";const c=s({__name:"Tag",props:{type:null},setup(r){return(e,l)=>(t(),n("span",{class:o(`
+      bg-frosty-span text-discortics-blue
+      text-xs
+      font-medium
+      inline-flex
+      items-center
+      px-1.5
+      py-0.5
+      rounded
+    `)},[a(e.$slots,"default")],2))}});export{c as _};
